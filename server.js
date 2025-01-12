cc4e74809180dfcb9a550f435b764fa93f2516e1
Peter Heyboer <heyboer.peter@gmail.com>
2025-01-11 21:35:44 -0500

    importing database connection and testing connection

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ require('dotenv').config();
 const sassMiddleware = require('./lib/sass-middleware');
 const express = require('express');
 const morgan = require('morgan');
+const db = require('./connection');  // Import the database connection
 
 const PORT = process.env.PORT || 8080;
 const app = express();
