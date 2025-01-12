79f95b3a7a183d5463fb3fbeeaf774b567a3e912
abbas <abbasbukhari2015@gmail.com>
2025-01-12 00:18:03 -0500

    Updated users and widgets seed files with relevant data

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,5 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (id, name) VALUES (1, 'Alice');
+INSERT INTO users (id, name) VALUES (2, 'Kira');
+INSERT INTO users (id, name) VALUES (3, 'John');
+INSERT INTO users (id, name) VALUES (4, 'Jane');
--- db/seeds/05_widgets.sql
+++ db/seeds/05_widgets.sql
@@ -1,4 +1,8 @@
--- Widgets table seeds here (Example)
-INSERT INTO widgets (name, user_id) VALUES ('Sprockets', 1);
-INSERT INTO widgets (name, user_id) VALUES ('Chains', 2);
-INSERT INTO widgets (name, user_id) VALUES ('Bearings', 2);
+-- Clear the table to avoid duplicates
+TRUNCATE TABLE widgets RESTART IDENTITY CASCADE;
+
+-- Insert relevant widgets for Schoodle
+INSERT INTO widgets (name, user_id) VALUES ('Event Calendar', 1);
+INSERT INTO widgets (name, user_id) VALUES ('Availability Poll', 2);
+INSERT INTO widgets (name, user_id) VALUES ('Response Summary', 3);
+INSERT INTO widgets (name, user_id) VALUES ('Notification Tool', 4);
