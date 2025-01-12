6a9ebff876a05faab8e28d94581c20ffc50f70cd
Peter Heyboer <heyboer.peter@gmail.com>
2025-01-12 18:19:40 -0500

    implementing POST route for creating new time slot

--- routes/time_slots.js
+++ routes/time_slots.js
@@ -2,3 +2,25 @@ const express = require("express");
 const db = require("../connection");
 const router = express.Router();
 
+// POST route  for creating a new time slot for an event /time_slots
+router.post("/", async (req, res) => {
+  const { event_id, start_time, end_time } = req.body;
+
+  // Input validation
+  if (!event_id || !start_time || !end_time) {
+    return res.status(400).json({ error: "All fields required" });
+  }
+
+  try {
+    const result = await db.query(
+      "INSERT INTO time_slots (event_id, start_time, end_time) VALUES ($1, $2, $3) RETURNING *",
+      [event_id, start_time, end_time]
+    );
+
+    const newTimeSlot = result.rows[0];
+    res.status(201).json(newTimeSlot); // Returns new time slot
+  } catch (error) {
+    console.error("Error Creating Time Slot:", error);
+    res.status(500).json({ error: "Server Error" });
+  }
+});
