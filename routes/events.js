cbc07514dea86b969538ccccce411a69404d5a2a
Peter Heyboer <heyboer.peter@gmail.com>
2025-01-12 00:03:20 -0500

    remade tables and added post route for creating new events

---
+++ routes/events.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const db = require("../connection");
+const router = express.Router();
+
+// Post route for creating a new event - POST /events
+router.post("/", async (req, res) => {
+  const { event_name, description, organizer_name, organizer_email } = req.body;
+
+  if (!event_name || !description || !organizer_name || organizer_email) {
+    return res
+      .status(400)
+      .json({ error: "All fields required to create an event" });
+  }
+
+  try {
+    const result = await db.query(
+      "INSERT INTO events (event_name, description, organizer_name, organizer_email) VALUES ($1, $2, $3, $4) RETURNING *",
+      [event_name, description, organizer_name, organizer_email]
+    );
+    const newEvent = result.rows[0];
+    res.status(201).json(newEvent);
+  } catch (error) {
+    console.error("Error creating event:", error);
+    res.status(500).json({ error: "Server error" });
+  }
+});
