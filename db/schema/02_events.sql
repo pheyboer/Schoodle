79cc8c4cdb1df5e7ebd336c70fe7068884e8274f
Peter Heyboer <heyboer.peter@gmail.com>
2025-01-12 20:17:57 -0500

    adding events table sql

---
+++ db/schema/02_events.sql
@@ -0,0 +1,7 @@
+CREATE TABLE public.events (
+    event_id SERIAL PRIMARY KEY,
+    event_name VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
+    organizer_name VARCHAR(255) NOT NULL,
+    organizer_email VARCHAR(255) NOT NULL
+);
