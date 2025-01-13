22d3b0a87ab0831b068bd8b2360c4d3b4ca52575
Peter Heyboer <heyboer.peter@gmail.com>
2025-01-13 15:46:12 -0500

    updating seed file for availability responses

--- db/seeds/06_seed_availability_responses.sql
+++ db/seeds/06_seed_availability_responses.sql
@@ -1,12 +1,12 @@
 -- Seed data for the availability_responses table - tesing
-INSERT INTO availability_responses (attendee_id, time_slot_id, event_id, availability)
+INSERT INTO availability_responses (attendee_id, time_slot_id, event_id)
 VALUES
-    (1, 1, 1, true),
-    (2, 1, 1, false),
-    (3, 1, 1, true),
-    (1, 2, 1, true),
-    (2, 2, 1, true),
-    (3, 2, 1, false),
-    (1, 3, 1, false),
-    (2, 3, 1, true),
-    (3, 3, 1, true);
+    (1, 1, 1),
+    (2, 1, 1),
+    (3, 1, 1),
+    (1, 2, 1),
+    (2, 2, 1),
+    (3, 2, 1),
+    (1, 3, 1),
+    (2, 3, 1),
+    (3, 3, 1);
