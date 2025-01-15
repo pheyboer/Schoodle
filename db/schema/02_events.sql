ad63768823b2789de41140afe8b5adda3eadaf9f
Peter Heyboer <heyboer.peter@gmail.com>
2025-01-15 18:57:42 -0500

    implementing unique URL

--- db/schema/02_events.sql
+++ db/schema/02_events.sql
@@ -5,5 +5,6 @@ CREATE TABLE events (
     event_name VARCHAR(255) NOT NULL,
     description TEXT NOT NULL,
     organizer_name VARCHAR(255) NOT NULL,
-    organizer_email VARCHAR(255) NOT NULL
+    organizer_email VARCHAR(255) NOT NULL,
+    unique_url TEXT UNIQUE
 );
