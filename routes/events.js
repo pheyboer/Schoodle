579b10652b66642ffbf391a668a049f81eb4ae0c
Abdullah Zia <amz1cflp99@gmail.com>
2025-01-15 17:32:33 -0500

    adjusted get route

--- routes/events.js
+++ routes/events.js
@@ -16,8 +16,8 @@ router.post("/", async (req, res) => {
 
   try {
     const result = await db.query(
-      "INSERT INTO events (event_name, description, organizer_name, organizer_email) VALUES ($1, $2, $3, $4) RETURNING *",
-      [event_name, description, organizer_name, organizer_email]
+      "INSERT INTO events (event_name, description, time_slots) VALUES ($1, $2, $3, $4) RETURNING *",
+      [name, description, timeSlots]
     );
     const newEvent = result.rows[0];
     res.status(201).json(newEvent);
@@ -32,9 +32,10 @@ router.get("/:id", async (req, res) => {
   const { id } = req.params;
 
   try {
-    const result = await db.query("SELECT * FROM events WHERE event_id = $1", [
-      id,
-    ]);
+    const result = await db.query(
+      "SELECT event_id AS id, event_name AS name, description, time_slots AS timeSlots FROM events WHERE event_id = $1", 
+      [id]
+    );
 
     if (result.rows.length === 0) {
       return res.status(404).json({ error: "Sorry, event not found." });
